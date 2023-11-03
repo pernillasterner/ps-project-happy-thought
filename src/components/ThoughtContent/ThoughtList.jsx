1ac080e7a52353e11daeb03ea8a6b92774ddd0b0
psterner <psterner@hotmail.com>
2023-11-03 20:14:59 +0100

    Change id to key. Needs to be unique key

--- src/components/ThoughtContent/ThoughtList.jsx
+++ src/components/ThoughtContent/ThoughtList.jsx
@@ -2,26 +2,43 @@
  * 1. Import styling file ✅
  * 2. Get the thoughts and render them in a list ✅
  * 3. Listen to the like button and add an onClick event to it will incease each time.
- * 4. At date-fns to update the time when the thought was created. Use date-fns that is more lightweight. Install npm install date-fns
+ * 4. At date-fns to update the time when the thought was created. Use date-fns that is more lightweight. Install npm install date-fns ✅
  */
+import { useState } from "react";
 import { formatDistanceToNow } from "date-fns";
 import { enUS } from "date-fns/locale";
 import "./ThoughtList.scss";
 
 export const ThoughtList = ({ thoughts }) => {
+  //   const { likes, setLikes } = useState(null);
+  const handleLikes = (hearts) => {
+    // THOUGHT_ID/like
+    // setLikes(hearts);
+  };
+
   return (
     <div className="list-wrapper">
       {thoughts.map((thought) => (
-        <div id={thought._id} className="message">
+        <div key={thought._id} className="message">
           <p>{thought.message}</p>
           <div className="info-wrapper">
             <div className="info-like">
-              <button id="likeBtn" className="like-button" type="button">
+              {/* add onClick to handle Likes */}
+              <button
+                id="likeBtn"
+                className="like-button"
+                type="button"
+                // onClick={setLikes}
+              >
                 <span className="emoji" aria-label="like button">
                   ❤️
                 </span>
               </button>
-              <span className="num-likes"> x {thought.hearts}</span>
+              {/* update the num of likes */}
+              <span className="num-likes">
+                {" "}
+                x {handleLikes(thought.hearts)}
+              </span>
             </div>
             <div className="info-time">
               {formatDistanceToNow(new Date(thought.createdAt), {
