f3a77284a6673ab1de16e472661749d38b5a6074
psterner <psterner@hotmail.com>
2023-11-03 19:40:49 +0100

    Add Post Request function to use in the Form

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -14,4 +14,29 @@ const makeGetRequest = async () => {
   }
 };
 
-export { makeGetRequest };
+const makePostRequest = async (thought) => {
+  try {
+    const res = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Add the body with data to post
+      body: JSON.stringify({
+        message: thought,
+      }),
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    } else {
+      throw new Error(`Failed to make a POST request: ${res.status}`);
+    }
+  } catch (error) {
+    console.error("Error making POST request:", error);
+    throw error;
+  }
+};
+
+export { makeGetRequest, makePostRequest };
