de4df5013a85fd2283d31652a120ed0ef174d48b
psterner <psterner@hotmail.com>
2023-11-03 11:49:10 +0100

    Import Header and ThoughtContent components. Also styling file

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@
  */
 import "./App.scss";
 import { Header } from "./Header";
+import { ThoughtContent } from "./components/ThoughtContent/ThoughtContent";
 
 export const App = () => {
   return (
     <div className="main-wrapper">
       <Header />
+      <ThoughtContent />
     </div>
   );
 };
