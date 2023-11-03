15aab761d93608cf7370b396b167c87ae1a6744d
psterner <psterner@hotmail.com>
2023-11-03 19:44:20 +0100

    Add submit handler and send message using post function

--- src/components/ThoughtContent/ThoughtForm.jsx
+++ src/components/ThoughtContent/ThoughtForm.jsx
@@ -1,7 +1,73 @@
+/**
+ * 1. Add template and styling ✅
+ * 2. Import makePostRequest function and post a request to the server
+ * 3. Handle validation errors
+ * 4. Check that the characters are more than 5 and less than 140.
+ * 5. Make 0 / 140 count up while typing
+ */
+import { useState } from "react";
+import { makePostRequest } from "../Api";
+import "./ThoughtForm.scss";
+
 export const ThoughtForm = () => {
+  const [messageContent, setMessageContent] = useState("");
+  const errorMessage = "";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Handle form validation
+    const validStringRegex = /^[A-Za-z0-9\s!.,?'"]*$/;
+
+    if (!validStringRegex.test(messageContent)) {
+      errorMessage = "The string contains invalid characters";
+    } else if (messageContent.length < 5) {
+      errorMessage =
+        "Your message is too short, it needs at least 5 letters 😔";
+    } else if (messageContent.length > 140) {
+      errorMessage = "Your message is too long 😔";
+    } else {
+      // Content is valid. Continue with post
+      const messages = makePostRequest(messageContent);
+      console.log(messages);
+    }
+
+    // Handle form submission
+  };
+
   return (
-    <>
-      <h2>Add form</h2>
-    </>
+    <div className="post-wrapper">
+      <h2>What is making you happy right now?</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <textarea
+            name="messageContent"
+            rows={3}
+            placeholder="'If music be the food of love, play on.' – William Shakespeare"
+            width={454}
+            height={76}
+            required={true}
+            onChange={(e) => setMessageContent(e.target.value)}
+          />
+        </label>
+        <div className="post-length">
+          <p className="error">{errorMessage}</p>
+          <p className="length">0 / 140</p>
+        </div>
+        <button
+          id="submitPostBtn"
+          type="submit"
+          aria-label="button for submitting your post"
+        >
+          <span className="emoji" aria-label="heart emoji">
+            ❤️
+          </span>
+          Send Happy Thought
+          <span className="emoji" aria-label="heart emoji">
+            ❤️
+          </span>
+        </button>
+      </form>
+    </div>
   );
 };
