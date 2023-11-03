ff03d5913d83e6c96ba1856dfe23512f3931ad42
psterner <psterner@hotmail.com>
2023-11-03 11:13:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/**
+ * Import ThoughtContent and Header components
+ */
+import "./App.scss";
+import { Header } from "./Header";
+
+export const App = () => {
+  return (
+    <div className="main-wrapper">
+      <Header />
+    </div>
+  );
+};
