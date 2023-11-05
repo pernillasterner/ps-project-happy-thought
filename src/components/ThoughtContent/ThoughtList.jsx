19f3bdf85e53e98b19de2292366142d9a7e3cb63
psterner <psterner@hotmail.com>
2023-11-05 16:44:37 +0100

    Create a function that will increase amount of likes when clicking on button

--- src/components/ThoughtContent/ThoughtList.jsx
+++ src/components/ThoughtContent/ThoughtList.jsx
@@ -4,16 +4,47 @@
  * 3. Listen to the like button and add an onClick event to it will incease each time.
  * 4. At date-fns to update the time when the thought was created. Use date-fns that is more lightweight. Install npm install date-fns ✅
  */
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { makeLikeRequest } from "../Api";
 import { formatDistanceToNow } from "date-fns";
 import { enUS } from "date-fns/locale";
 import "./ThoughtList.scss";
 
 export const ThoughtList = ({ thoughts }) => {
-  //   const { likes, setLikes } = useState(null);
-  const handleLikes = (hearts) => {
-    // THOUGHT_ID/like
-    // setLikes(hearts);
+  const [likes, setLikes] = useState({});
+
+  useEffect(() => {
+    // Initialize likes from the API
+    const initialLikes = {};
+    thoughts.forEach((thought) => {
+      initialLikes[thought._id] = thought.hearts;
+    });
+    setLikes(initialLikes);
+  }, [thoughts]);
+
+  const handleLikes = async (id) => {
+    try {
+      // Send a POST request to update the likes
+      await makeLikeRequest(id);
+
+      // Update the likes in component state
+      setLikes((prevLikes) => ({
+        ...prevLikes,
+        [id]: prevLikes[id] + 1,
+      }));
+    } catch (err) {
+      console.error("Error updating likes", err);
+    }
+  };
+
+  const checkNewThought = (thought) => {
+    // Thought timestamp
+    const thoughtTimestamp = new Date(thought.createdAt);
+    const currentTime = new Date();
+    // Calculate the difference in milliseconds
+    const timeDiff = currentTime - thoughtTimestamp;
+    // Check if message was created less than a minute ago
+    return timeDiff <= 60000;
   };
 
   return (
@@ -26,9 +57,11 @@ export const ThoughtList = ({ thoughts }) => {
               {/* add onClick to handle Likes */}
               <button
                 id="likeBtn"
-                className="like-button"
+                onClick={() => handleLikes(thought._id)}
+                className={`like-button ${
+                  checkNewThought(thought) ? "new-thought" : ""
+                }`}
                 type="button"
-                // onClick={setLikes}
               >
                 <span className="emoji" aria-label="like button">
                   ❤️
@@ -36,8 +69,7 @@ export const ThoughtList = ({ thoughts }) => {
               </button>
               {/* update the num of likes */}
               <span className="num-likes">
-                {" "}
-                x {handleLikes(thought.hearts)}
+                {thought.hearts + likes[thought._id]}x
               </span>
             </div>
             <div className="info-time">
